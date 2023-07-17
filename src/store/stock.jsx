01c92dd4f0a2d86c17c8335a2bda1cf6ecc68176
Даниил Кулешов <daniil42rus@mail.ru>
2023-07-17 15:31:21 +0700

    stock 0

--- src/store/stock.jsx
+++ src/store/stock.jsx
@@ -7,19 +7,19 @@ class Stock {
       id: 1,
       name: 'Биорука',
       price: 5,
-      item: 4,
+      item: 0,
     },
     {
       id: 2,
       name: 'Микрочип',
       price: 3,
-      item: 4,
+      item: 0,
     },
     {
       id: 3,
       name: 'Душа',
       price: 15,
-      item: 3,
+      item: 0,
     },
   ];
 
